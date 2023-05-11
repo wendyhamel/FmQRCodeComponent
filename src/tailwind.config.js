10ede367e64c2a841a7711d4f3bb28bae13a3559
wendyhamel <whamel@it4web.nl>
2023-05-11 13:01:45 +0200

    Styling card

---
+++ src/tailwind.config.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+	theme: {
+		fontFamily: {
+			'sans': ['Outfit', 'sans-serif'],
+		},
+		extend: {
+			colors: {
+				'white': 'hsl(0, 0%, 100%)',
+				'light-gray': 'hsl(212, 45%, 89%)',
+				'gray-blue': 'hsl(220, 15%, 55%)',
+				'dark-blue': 'hsl(218, 44%, 22%)',
+			},
+			'fontSize': {
+				'small': '0.9375rem',
+				'large': '1.375rem'
+			}
+		}
+	}
+}
